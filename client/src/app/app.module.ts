2f0a95a5a273f07fe5369cb471226d73f4853047
Daniel Glazer <danielglazer123@gmail.com>
2020-09-17 11:41:40 +0300

    build(dependencies): add libraries as dependencies to app
    The libraries I added are:
    @angular/flex-layout
    @angular/material
    @ngrx/effects
    @ngrx/store

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { QuestionComponent } from './components/question/question.component';
 import { AnswerComponent } from './components/question/answer/answer.component';
 import { LeaderBoardComponent } from './components/leader-board/leader-board.component';
 import { GameComponent } from './components/game/game.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,10 @@ import { GameComponent } from './components/game/game.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
